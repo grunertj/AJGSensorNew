2b84c5ec3f17b8577e91bd8a824f76fd80b4286b
grunertj <grunertj@gmail.com>
2022-07-01 18:47:42 +0200

    new

--- app/src/main/java/com/example/ajgsensor/MainActivity.java
+++ app/src/main/java/com/example/ajgsensor/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Context;
 import android.hardware.Sensor;
