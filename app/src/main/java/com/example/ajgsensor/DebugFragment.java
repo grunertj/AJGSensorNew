b4408aab5d4a7580e22d2cc65f14d37dab8a67b5
grunertj <grunertj@gmail.com>
2022-07-01 14:50:35 +0200

    new

--- app/src/main/java/com/example/ajgsensor/DebugFragment.java
+++ app/src/main/java/com/example/ajgsensor/DebugFragment.java
@@ -1,12 +1,25 @@
 package com.example.ajgsensor;
 
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +27,10 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class DebugFragment extends Fragment {
+    Button buttonSensors, buttonShare;
+    TextView textViewSensors;
+    SensorManager sensorManager = null;
+    String phone = Build.MODEL+" "+Build.BRAND+" "+Build.MANUFACTURER+" "+Build.VERSION.RELEASE;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +76,70 @@ public class DebugFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_debug, container, false);
+        // return inflater.inflate(R.layout.fragment_debug, container, false);
+        View view = inflater.inflate(R.layout.fragment_debug, container, false);
+        textViewSensors = (TextView) view.findViewById(R.id.textViewSensors);
+        buttonSensors = (Button) view.findViewById(R.id.buttonSensors);
+        buttonShare = (Button) view.findViewById(R.id.buttonShare);
+        buttonShare.setEnabled(false);
+
+        buttonSensors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewSensors.setText(""+phone+"\n");
+                sensorManager = (SensorManager)getActivity().getSystemService(Context.SENSOR_SERVICE);
+                List<Sensor> list = sensorManager.getSensorList(Sensor.TYPE_ALL);
+                ArrayList<Sensor> sensors = new ArrayList<Sensor>();
+                sensors.addAll(list);
+
+                Collections.sort(sensors, new Comparator<Sensor>() {
+                    @Override
+                    public int compare(Sensor lhs, Sensor rhs) {
+                        return lhs.getType() - rhs.getType();
+                    }
+                });
+
+                for (Sensor sensor: sensors) {
+                    textViewSensors.append(sensor.getType()+"\n\t"+sensor.getName() + "\n\t"+sensor.getVendor()+"\n\n");
+                }
+                sensorManager = null;
+                buttonShare.setEnabled(true);
+            }
+        });
+
+        buttonShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                String string = textViewSensors.getText().toString();
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Phone: "+phone);
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, string);
+                startActivity(sharingIntent);
+            }
+        });
+
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    MainActivity.dim_state = MainActivity.dim_screen(false, getActivity());
+                }
+                return false;
+            }
+        });
+
+        return view;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser && isResumed()) {
+            MainActivity.dim_screen(false, getActivity());
+        } else if (isResumed()) {
+
+        }
     }
 }
--- app/src/main/java/com/example/ajgsensor/MainActivity.java
+++ app/src/main/java/com/example/ajgsensor/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.ajgsensor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 
 import com.google.android.material.tabs.TabLayout;
@@ -53,6 +59,133 @@ public class MainActivity extends AppCompatActivity {
         log[Sensor.TYPE_GYROSCOPE_UNCALIBRATED] = false;
         log[Sensor.TYPE_ROTATION_VECTOR] = false;
 
+        if (mainFragment == null) {
+            mainFragment = new MainFragment();
+        }
 
+        if (mainFragment == null) {
+            mainFragment = new MainFragment();
+            mainFragment.setRetainInstance(true);
+        }
+
+        if (settingsFragment == null) {
+            settingsFragment = new SettingsFragment();
+            settingsFragment.setRetainInstance(true);
+        }
+
+        if (debugFragment == null) {
+            debugFragment = new DebugFragment();
+            debugFragment.setRetainInstance(true);
+        }
+
+        global_fragments = new String[3];
+        global_fragments[0] = "Main";
+        global_fragments[1] = "Settings";
+        global_fragments[2] = "Debug";
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(new CustomAdapter(getSupportFragmentManager(), getApplicationContext()));
+        viewPager.setOffscreenPageLimit(100);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+        });
+    }
+
+    private class CustomAdapter extends FragmentPagerAdapter {
+        private String fragments [] = global_fragments;
+
+        public CustomAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public CustomAdapter(FragmentManager fm, Context applicationContext) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return mainFragment;
+                case 1:
+                    return settingsFragment;
+                case 2:
+                    return debugFragment;
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragments[position];
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            super.destroyItem(container, position, object);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            curBrightnessValue = Settings.System.getInt(getContentResolver(),Settings.System.SCREEN_BRIGHTNESS);
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static public boolean dim_screen (boolean on, Context context) {
+        if (on == true) {
+            try {
+                try {
+                    curBrightnessValue = android.provider.Settings.System.getInt(context.getContentResolver(),Settings.System.SCREEN_BRIGHTNESS);
+                } catch (Settings.SettingNotFoundException e) {
+                    e.printStackTrace();
+                }
+                Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+                Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 0);
+            } catch (Exception e) {}
+            return false;
+        } else {
+            try {
+                Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+                if (curBrightnessValue > 64) {
+                    Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, curBrightnessValue);
+                } else {
+                    Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 64);
+                }
+            } catch (Exception e) {}
+            return  true;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        dim_screen(false,this);
     }
 }
